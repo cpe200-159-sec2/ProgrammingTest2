a6611d485cdbdb23f776205ee829155dfbd96797
GGG <mayni_cb@hotmail.com>
2016-11-18 16:29:40 +0700

    test2

--- src/Registration.java
+++ src/Registration.java
@@ -7,37 +7,77 @@ public class Registration {
     // Add new course, only request with non-exists studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void add(IRequest request) {
+        if(request.getRequestType()==IRequest.REQUEST_ADD){
+            if(exists(request)==false){
+                requestList.add(request);
+            }else{
+                throw new RuntimeException("SAME");
+            }
+        }else{
+            throw new RuntimeException("Enter new type");
+        }
 
     }
 
     // Drop existing course, only request with existing studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void drop(IRequest request) {
+        if(request.getRequestType()==IRequest.REQUEST_DROP){
+            if(exists(request)==true){
+                requestList.remove(request);
+            }else{
+                throw new RuntimeException("Don't have this request");
+            }
+
+        }else{
+            throw new RuntimeException("Enter new type");
+        }
 
     }
 
     // Check whether the request is in repository.
     public boolean exists(IRequest request) {
-        return false;
+
+        return requestList.contains(request);
     }
 
     // Count all requests
     public int count() {
-        return 0;
+        return requestList.size();
     }
 
     // Count requests by request type
     public int countByType(int type) {
-        return 0;
+        int c=0;
+
+        for(int i=0;i<requestList.size();i++){
+            if(requestList.get(i).getRequestType()==type){
+                c=c+1;
+            }
+        }
+
+        return c;
     }
 
     // Count request by course ID
     public int countByCourseID(String courseID) {
-        return 0;
+        int c=0;
+        for(int i=0;i<requestList.size();i++){
+            if(requestList.get(i).getCourseID().equals(courseID)){
+                c=c+1;
+            }
+        }
+        return c;
     }
 
     // Count request by student ID
     public int countByStudentID(String studentID) {
-        return 0;
+        int c=0;
+        for(int i=0;i<requestList.size();i++){
+            if(requestList.get(i).getStudentID().equals(studentID)){
+                c=c+1;
+            }
+        }
+        return c;
     }
 }
