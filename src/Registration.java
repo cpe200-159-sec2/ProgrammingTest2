140c5300a8fa4f29edfa3eac5f489d4a88072079
GGG <mayni_cb@hotmail.com>
2016-11-18 17:10:06 +0700

    test2

--- src/Registration.java
+++ src/Registration.java
@@ -3,12 +3,15 @@ import java.util.ArrayList;
 public class Registration {
     // Request repository
     private ArrayList<IRequest> requestList;
+    public Registration(){
+        requestList=new ArrayList<>();
+    }
 
     // Add new course, only request with non-exists studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void add(IRequest request) {
         if(request.getRequestType()==IRequest.REQUEST_ADD){
-            if(exists(request)==false){
+            if(!exists(request)){
                 requestList.add(request);
             }else{
                 throw new RuntimeException("SAME");
@@ -22,17 +25,14 @@ public class Registration {
     // Drop existing course, only request with existing studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void drop(IRequest request) {
-        if(request.getRequestType()==IRequest.REQUEST_DROP){
-            if(exists(request)==true){
-                requestList.remove(request);
-            }else{
-                throw new RuntimeException("Don't have this request");
+        for(int i=0;i<requestList.size();i++){
+            if(requestList.get(i).getStudentID()==request.getStudentID()&&requestList.get(i).getCourseID()==request.getCourseID()){
+                    if(requestList.get(i).getRequestType()==IRequest.REQUEST_DROP){
+                    requestList.remove(request);
+                }
             }
-
-        }else{
-            throw new RuntimeException("Enter new type");
         }
-
+        throw new RuntimeException("Don't have this request");
     }
 
     // Check whether the request is in repository.
@@ -73,11 +73,18 @@ public class Registration {
     // Count request by student ID
     public int countByStudentID(String studentID) {
         int c=0;
-        for(int i=0;i<requestList.size();i++){
-            if(requestList.get(i).getStudentID().equals(studentID)){
+        for(IRequest r : requestList) {
+            if(r.getStudentID().equals(studentID)) {
                 c=c+1;
             }
         }
         return c;
+
+        /*for(int i=0;i<requestList.size();i++){
+            if(requestList.get(i).getStudentID().equals(studentID)){
+                c=c+1;
+            }
+        }
+        return c;*/
     }
 }
