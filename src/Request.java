dea085882d51033685ece22c62654e289015c78c
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-11-18 14:30:30 +0700

    commit 1

--- src/Request.java
+++ src/Request.java
@@ -3,6 +3,10 @@
  */
 public class Request implements IRequest {
 
+    private int type;           // type of add or drop
+    private String studentID;
+    private String courseID;
+
     public static IRequest createRequest(int type, String studentID, String courseID) {
         return null;
     }
@@ -14,26 +18,45 @@ public class Request implements IRequest {
 
     @Override
     public String getStudentID() {
-        return null;
+        return studentID;
     }
 
     @Override
     public String setStudentID(String studentID) {
-        return null;
+        String regex = "5+[0-9][0-6]{0,9}"; //ex 580610615
+        if(studentID == null){
+            return null;
+        }else{
+            if(studentID.matches(regex)){
+                throw new RuntimeException("Please type corrected input.");
+            }else{
+                this.studentID = studentID;
+                return studentID;
+            }
+        }
     }
 
     @Override
     public String getCourseID() {
-        return null;
+        return courseID;
     }
 
     @Override
     public void setCourseID(String courseID) {
-
+        String regex = "[0-9]{3,6}";    //ex: 001201
+        if(courseID == null){
+            throw new RuntimeException("Empty courseID.");
+        }else{
+            if(courseID.matches(regex)){
+                throw new RuntimeException("Please enter corrected courseID.");
+            }else{
+                this.courseID = courseID;
+            }
+        }
     }
 
     @Override
     public int getRequestType() {
-        return 0;
+        return type;
     }
 }
