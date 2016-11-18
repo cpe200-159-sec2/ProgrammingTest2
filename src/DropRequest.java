f80ffdd5f1abc257bc8e24fd4aa5d47fd8e869e9
GEST <naluedomgest@gmail.com>
2016-11-18 15:38:22 +0700

    :)

--- src/DropRequest.java
+++ src/DropRequest.java
@@ -1,4 +1,5 @@
 public class DropRequest extends Request {
-    private DropRequest() {
+    private DropRequest(String studentID,String courseID) {
+        createRequest(REQUEST_DROP,studentID,courseID);
     }
 }
