9291852ddfe3d454a4c4bac8e383d129ccb57515
Nachpon <nachpon.petai@gmail.com>
2016-11-18 15:38:37 +0700

    Regis

--- src/Registration.java
+++ src/Registration.java
@@ -2,42 +2,59 @@ import java.util.ArrayList;
 
 public class Registration {
     // Request repository
-    private ArrayList<IRequest> requestList;
+    private ArrayList<IRequest> requestList = new ArrayList<>();
 
     // Add new course, only request with non-exists studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void add(IRequest request) {
-
+        requestList.add(request);
     }
 
     // Drop existing course, only request with existing studentID/courseID
     // is a valid request. Invalid request will cause an exception
     public void drop(IRequest request) {
-
+        if(!exists(request))throw new RuntimeException("Invalid request");
+        requestList.remove(request);
     }
 
     // Check whether the request is in repository.
     public boolean exists(IRequest request) {
-        return false;
+        if(requestList.contains(request)) return true;
+        else return false;
     }
 
     // Count all requests
     public int count() {
-        return 0;
+        return requestList.size();
     }
 
     // Count requests by request type
     public int countByType(int type) {
-        return 0;
+        int count  = 0;
+        for(IRequest x: requestList){
+            if(x.getRequestType() == type){
+                count++;
+            }
+        }return  count;
     }
 
     // Count request by course ID
     public int countByCourseID(String courseID) {
-        return 0;
+        int count  = 0;
+        for(IRequest x: requestList){
+            if(x.getCourseID().equals(courseID)){
+                count++;
+            }
+        }return  count;
     }
 
     // Count request by student ID
     public int countByStudentID(String studentID) {
-        return 0;
+        int count  = 0;
+        for(IRequest x: requestList){
+            if(x.getStudentID().equals(studentID)){
+                count++;
+            }
+        }return  count;
     }
 }
